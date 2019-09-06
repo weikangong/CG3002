9eaa3235af7ae204989595e39449fd8860e4f918
weikangong <ongwk95@hotmail.com>
2019-09-06 15:22:28 +0800

    init client.py

---
+++ comms/client.py
@@ -0,0 +1,21 @@
+import serial
+import time
+
+class Raspberry():
+        def main(self):
+                #set up port connection
+                self.port=serial.Serial("/dev/serial0", baudrate=115200)
+                self.port.reset_input_buffer()
+                self.port.reset_output_buffer()
+
+                #Handshaking, keep saying 'H' to Arduino unitl Arduion reply 'A'
+                while(self.port.in_waiting == 0 or self.port.read() != 'A'):
+                    print ('Try to connect to Arduino')
+                    self.port.write('S')
+                    time.sleep(1)
+                self.port.write('A');
+                print ('Connected')
+        
+if __name__ == '__main__':
+        pi = Raspberry()
+        pi.main()
