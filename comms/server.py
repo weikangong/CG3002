6bfcb43648a55d59a20a5f275b77d9bd82fb11df
DESKTOP-79RE7G8\Justus Wah <e0032368@u.nus.edu>
2019-09-08 14:52:14 +0800

    working prototype, added server code

---
+++ comms/server.py
@@ -0,0 +1,27 @@
+import socket
+print("hi")
+
+ip_address = "192.168.1.175"    
+port_number = 8080
+    
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("socket created")
+    
+#initialize server 
+server_address = (ip_address, port_number)
+print('starting on '+ ip_address+ ' port: '+ str(port_number))
+        
+try:
+    sock.bind(server_address)
+except socket.error as msg:
+    print(msg)
+print("Socket bind complete.")
+
+#Listen for connections
+
+sock.listen(1)
+connection, address = sock.accept()
+print("connected to "+address[0] + ":" + str(address[1]))
+
+data = connection.recv(1024)
+print(data)
