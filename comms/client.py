eb5dc1530b36faf8bf0fe36e111a4d0263c08771
DESKTOP-79RE7G8\Justus Wah <e0032368@u.nus.edu>
2019-09-13 15:14:12 +0800

    added server ip and host parameters

--- comms/client.py
+++ comms/client.py
@@ -3,11 +3,18 @@ import time
 import socket
 
 class Raspberry():
+        def __init__(self):
+                self.socket = []
+                self.powerList = [0, 0, 0, 0]
+
         def main(self):
                 #set up port connection
                 self.port=serial.Serial("/dev/serial0", baudrate=115200)
                 self.port.reset_input_buffer()
                 self.port.reset_output_buffer()
+
+                #wei kang, i commented this out for testing only
+                '''
 
                 #Handshaking, keep saying 'H' to Arduino unitl Arduion reply 'A'
                 while(self.port.in_waiting == 0 or self.port.read() != 'A'):
@@ -17,10 +24,24 @@ class Raspberry():
                 self.port.write('A');
                 print ('Connected')
 
-        def connectToServer(self):
+                '''
+
+                try:
+                        self.setUpComms()
+                        self.connectToServer(self.socket[0], self.socket[1])
+                except KeyboardInterrupt:
+                        sys.exit(1)
+
+        def setUpComms(self):
+                print ("Enter ip address: ")
+                self.socket.append(sys.argv[1])
+                print ("Enter port number: ")
+                self.socket.append(sys.argv[2])
+
+        def connectToServer(self, host, port):
                 print("attempting to connect to server")
-                self.HOST = "192.168.43.203"
-                self.PORT = 8080
+                self.HOST = host #"192.168.43.203"
+                self.PORT = int(port) #8080
                 self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.s.connect((self.HOST, self.PORT))
                 print("connected")
@@ -34,5 +55,4 @@ class Raspberry():
 
 if __name__ == '__main__':
         pi = Raspberry()
-        # pi.main(), temp remove for testing
-        pi.connectToServer()
+        pi.main()
