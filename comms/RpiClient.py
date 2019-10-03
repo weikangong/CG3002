59e3d4c3eb317b3527449314423398e2da34478d
weikangong <ongwk95@hotmail.com>
2019-10-03 16:48:58 +0800

    added encryption + final server connection

--- comms/RpiClient.py
+++ comms/RpiClient.py
@@ -5,8 +5,23 @@ import sys
 import threading
 import time
 import CircularBuffer
+import random
 
+from Crypto.Cipher import AES
+from Crypto import Random
+from Crypto.Util.py3compat import *
+
+import base64
+import numpy as np
+#import pandas as pd
+import math
+
+#global variables
 mutex = threading.Lock()
+voltage = 0
+current = 0
+power = 0
+cum_power = 0
 
 class ReceiveData(threading.Thread):
         def __init__(self, buffer, port, period, packetSize):
@@ -47,12 +62,35 @@ class ReceiveData(threading.Thread):
 class clientComms():
         def __init__(self):
                 self.socket = []
+                self.SECRET_KEY = "panickerpanicker"
+                self.actions = ['handmotor', 'bunny', 'tapshoulder', 'rocket', 'cowboy', 'hunchback', 'jamesbond','chicken', 'movingsalute', 'whip', 'logout']
+                
 
         def main(self):
                 try:
                         self.setUpComms()
                         self.connectToServer(self.socket[0], self.socket[1])
-                        self.sendMessage("test message")       #change this to be input
+                        
+                        time.sleep(3)
+                        self.connectToServer(self.socket[0], self.socket[1])
+                        
+                        print("all good so far")
+                        
+                        while True:
+                            
+                            iv = Random.new().read(AES.block_size)
+                            cipher = AES.new(self.SECRET_KEY, AES.MODE_CBC, iv)
+                            randomMove = random.randint(0,9)
+                            message = ("#" + self.actions[randomMove]+ "|"+str(format(voltage, '.2f')) + "|" + str(format(current, '.2f')) + "|" + str(format(power, '.2f')) + "|" + str(format(cum_power, '.2f')) + "|").encode('utf8').strip()                             
+                            paddedMessage = self.padMessage(message, AES.block_size)
+                            encryptedMessage = cipher.encrypt(paddedMessage)
+                            encodedMessage = base64.b64encode(iv + encryptedMessage)
+                            time.sleep(3)
+                            print(self.actions[randomMove])
+                            self.sendMessage(encodedMessage)       #change this to be input
+                        
+                       
+                        
                 except KeyboardInterrupt:
                         sys.exit(1)
 
@@ -67,6 +105,19 @@ class clientComms():
                 self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 self.s.connect((self.HOST, self.PORT))
                 print("connected to server "+self.HOST+", port: "+str(self.PORT))
+                
+        def padMessage(self, payload, block_size, style = 'pkcs7'):
+            padding_len = block_size - len(payload) % block_size
+            if style == 'pkcs7':
+                padding = bchr(padding_len) * padding_len
+            elif style == 'x923':
+                padding = bchr(0) * (padding_len-1) + bchr(padding_len)
+            elif style == 'iso786':
+                padding = bchr(128) + bchr(0) * (padding_len - 1)
+            else:
+                raise ValueError("Unknown Padding STyle")
+        
+            return payload + padding
 
         def sendMessage(self, text):
                 try:
@@ -102,7 +153,7 @@ class Raspberry():
                     thread.start()
 
 if __name__ == '__main__':
-        pi = Raspberry()
-        pi.main()
-        # client = clientComms()
-        # client.main()
+        #pi = Raspberry()
+        #pi.main()
+        client = clientComms()
+        client.main()
