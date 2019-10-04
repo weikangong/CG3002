8856b2a717ee0fb3d80d7793e6d3e1c8952b26bb
weikangong <ongwk95@hotmail.com>
2019-10-04 16:50:06 +0800

    final working code

--- comms/CircularBuffer.py
+++ comms/CircularBuffer.py
@@ -8,20 +8,25 @@ class CircularBuffer():
 
         def put(self, packet):
                 result = [x.rstrip('\x00') for x in packet.split(',')]
-                print (result)
+                #print (result)
+                print (self.nextID)
+                print (str(int(result[0])))
                 if self.nextID == int(result[0]):
                         if self.ackID == (self.nextID + 1) % self.size:
                                 self.full = True
                                 print("buffer full")
                         else:
                                 self.buffer[self.nextID] = result
+                                print ("buffer saving: "+str(self.buffer[self.nextID]))
                                 self.nextID = (self.nextID + 1) % self.size
 
         # Returns list of packets ordered from old to new
         def get(self):
                 if(self.ackID <= self.nextID):
+                        print ("buffer get: "+str(self.buffer[self.ackID:self.nextID]))
                         return self.buffer[self.ackID:self.nextID]
                 else:
+                        print ("buffer get: "+str(self.buffer[self.ackID:self.size] + self.buffer[:self.nextID]))
                         return self.buffer[self.ackID:self.size] + self.buffer[:self.nextID]
 
         def ack(self, index):
